07e7c36ed056ab85f91be302b16d6004ede441f6
79268 <jhgjvhg>
2022-07-16 22:26:37 +0300

    Initial commit

---
+++ geektodo/frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+      <div>
+      <h1>
+        HELLO WORLD IT IS THE TO DO FOOTERRRR
+      </h1>
+      </div>
+  );
+};
+export default Footer;
---
+++ geektodo/frontend/src/components/Sitemenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Sitemenu = () => {
+  return (
+      <div>
+      <h1>
+        HELLO WORLD IT IS THE TO DO MENU
+      </h1>
+      </div>
+  );
+};
+export default Sitemenu;
